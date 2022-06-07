76f33e0cebac25571af265742c1151a97a39b6d5
Bianca Maciel <61274369+BiancaMMatos@users.noreply.github.com>
2022-06-07 14:05:10 -0300

    file structure

---
+++ QARCode/OnBoardingFeat/OnBoardingViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OnBoardingViewController.swift
+//  QARCode
+//
+//  Created by Bianca Maciel Matos on 07/06/22.
+//
+
+import Foundation
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+    
+}
