e71fd47a36ddb47884b6fb66b69b9059aaa000b6
Marcos Chevis <marcoschevism@gmail.com>
2022-06-08 16:36:04 -0300

    QRCodeTracker

--- QARCode/ARHologramFeat/ARHologramViewController.swift
+++ QARCode/ARHologramFeat/ARHologramViewController.swift
@@ -18,10 +18,10 @@ final class ARHologramViewController: UIViewController {
     var cgImage: CGImage?
     
     var sceneView: ARSCNView = {
-        let s: ARSCNView = .init(frame: .zero)//ARSCNView(frame: .zero)
+        let s: ARSCNView = .init(frame: .zero)
         s.translatesAutoresizingMaskIntoConstraints = false
         s.isUserInteractionEnabled = true
-        s.debugOptions = .showWireframe
+//        s.debugOptions = .showWireframe
 
         return s
     }()
@@ -39,8 +39,7 @@ final class ARHologramViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = .red
+
         sceneView.delegate = self
         setupConstraints()
     }
@@ -52,32 +51,15 @@ final class ARHologramViewController: UIViewController {
         print(cgImage.width)
         con.trackingImages = [ARReferenceImage(cgImage, orientation: .up, physicalWidth: 6.5)]
        
-        
         sceneView.session.run(con)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-//        sceneView.session.pause()
     }
     
     func setupConstraints() {
         self.view.addSubview(sceneView)
-        
-//        if let cgImage = cgImage {
-//            let im = UIImage(cgImage: cgImage)
-//            let v = UIImageView(image: im)
-//            v.contentMode = .scaleAspectFit
-//            v.translatesAutoresizingMaskIntoConstraints = false
-//            self.view.addSubview(v)
-////            v.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-////            v.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-//            v.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-//            v.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-//            v.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-//            v.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-//        }
-        
         
         
         
@@ -94,14 +76,14 @@ final class ARHologramViewController: UIViewController {
         
 
         DispatchQueue.main.async {
-            let arVC = UIHostingController(rootView: WebView(url: url))
+            let arVC = WebViewController(url: url)
             arVC.willMove(toParent: self)
             // make the hosting VC a child to the main view controller
             self.addChild(arVC)
             
             // set the pixel size of the Card View
             arVC.view.frame = CGRect(x: 0, y: 0, width: 500, height: 500)
-            arVC.view.layer.zPosition = 1000
+//            arVC.view.layer.zPosition = 1000
             
             // add the ar card view as a subview to the main view
             self.view.addSubview(arVC.view)
@@ -129,7 +111,7 @@ final class ARHologramViewController: UIViewController {
         viewController.view.backgroundColor = UIColor.clear
     }
     
-    func show(hostingVC: UIHostingController<WebView>, on node: SCNNode) {
+    func show(hostingVC: UIViewController, on node: SCNNode) {
         // create a new material
         let material = SCNMaterial()
         
@@ -156,19 +138,15 @@ extension ARHologramViewController: ARSCNViewDelegate {
         let plane = SCNPlane(width: CGFloat(planeAnchor.referenceImage.physicalSize.width),
                              height: CGFloat(planeAnchor.referenceImage.physicalSize.height))
         
-//        let planeNode = SCNNode(geometry: plane)
-        
-        
-        let pyramid = SCNPyramid(width: plane.width, height: plane.height, length: plane.height)
+        let pyramid = SCNPyramid(width: plane.width*1.1, height: plane.height, length: plane.height*1.1)
         let pyramidNode = SCNNode(geometry: pyramid)
         
-        let mat = SCNMaterial()
-//        mat.diffuse.contents =
-//        mat.selfIllumination.contents = UIColor.red
-        pyramidNode.geometry?.materials.first?.diffuse.contents = UIColor.blue.withAlphaComponent(0.5)
-//        pyramidNode.geometry?.materials.first?.selfIllumination
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.blue.withAlphaComponent(0.5)
         
-        let viewPlane = SCNPlane(width: pyramid.width, height: pyramid.height)
+        pyramidNode.geometry?.materials = [material]
+        
+        let viewPlane = SCNPlane(width: pyramid.width*0.9, height: pyramid.height*0.9)
         let viewPlaneNode = SCNNode(geometry: viewPlane)
 
         
@@ -181,10 +159,10 @@ extension ARHologramViewController: ARSCNViewDelegate {
         viewPlaneNode.eulerAngles.x = .pi/2
         viewPlaneNode.position.y -= 0.3
 
-        
         pyramidNode.position.y = Float(pyramid.length)
         
         createHostingController(for: viewPlaneNode)
+        
         
         pyramidNode.addChildNode(viewPlaneNode)
         node.addChildNode(pyramidNode)
@@ -194,19 +172,6 @@ extension ARHologramViewController: ARSCNViewDelegate {
     }
 }
 
-import WebKit
  
-struct WebView: UIViewRepresentable {
- 
-    var url: URL
- 
-    func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
-    }
- 
-    func updateUIView(_ webView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        webView.load(request)
-    }
-}
+
 
--- QARCode/CodeReaderFeat/CodeReaderViewController.swift
+++ QARCode/CodeReaderFeat/CodeReaderViewController.swift
@@ -7,17 +7,258 @@
 
 import UIKit
 
-class CodeReaderViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class CodeReaderViewController: UIViewController {
+    
+    struct MetadataInfo {
+        var corners: [CGPoint]
+        var string: String
     }
     
+    var captureSession: AVCaptureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    private let photoOutput = AVCapturePhotoOutput()
+//    var detectionOverlays: [CALayer] = []
+    
+    var capturedImage: CGImage?
+    var finalImage: CGImage?
+    var finalQRCodeRect: CGRect = .init()
+    
+    var qrCodes: [MetadataInfo] = []
+    var finalQRCode: MetadataInfo?
+    
+    
+    var detectionSublayer: CALayer?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.black
+        
+        setup()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if (captureSession.isRunning == false) {
+            captureSession.startRunning()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        if (captureSession.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
+    
+    private func setup() {
+        setupCaptureSession()
+        setupPreviewLayer()
+        
+        captureSession.startRunning()
+    }
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            fatalError()
+        }
+        
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            
+            return
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            captureSession.addOutput(photoOutput)
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+            captureSession.commitConfiguration()
+        } else {
+            
+            return
+        }
+    }
+    
+    private func setupPreviewLayer() {
+        self.previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        guard let previewLayer = previewLayer else { return }
+        
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        
+        view.layer.addSublayer(previewLayer)
+    }
+    
+    private func qrCodeMarkerPreviewLayer(qrCodes: [MetadataInfo]) {
+        
+    }
+    
+    func found(qrCodeMetadata: MetadataInfo) {
+        
+        self.finalQRCode = qrCodeMetadata
+        self.photoOutput.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            if let qrCodeMetadata = getQRCodeIn(touchLocation: touch.location(in: self.view), qrCodesMetadata: qrCodes) {
+                
+                found(qrCodeMetadata: qrCodeMetadata)
+                return
+            }
+        }
+        print("oh")
+    }
+    private func getQRCodeIn(touchLocation: CGPoint, qrCodesMetadata: [MetadataInfo]) -> MetadataInfo? {
+        for qrCodeMetadata in qrCodesMetadata {
+            if isPointInRect(point: touchLocation, rect: translateCornersSpaceIntoViewSpace(corners: qrCodeMetadata.corners)) {
+                return qrCodeMetadata
+            }
+            return nil
+        }
+        
+        return nil
+    }
+    private func translateCornersSpaceIntoViewSpace(corners: [CGPoint]) -> CGRect {
+       let newCorners = corners.map({ point -> CGPoint in
+            var newPoint = point
+            newPoint.x *= CGFloat(self.view.frame.height)
+            newPoint.y *= CGFloat(self.view.frame.width)
+            return newPoint
+        })
+        
+        return CGRect(x: self.view.frame.width - newCorners[0].y,
+                      y: newCorners[0].x,
+                      width: (newCorners[0].y - newCorners[2].y) ,
+                      height: (newCorners[2].x - newCorners[0].x))
 
-    /*
-    // MARK: - Navigation
+    }
+    private func isPointInRect(point: CGPoint, rect: CGRect) -> Bool {
+        if (point.x > rect.minX && point.x < rect.maxX) && (point.y > rect.minY && point.y < rect.maxY) {
+            return true
+        }
+        
+        return false
+    }
+    
+}
+
+extension CodeReaderViewController: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        qrCodes = []
+        
+        for metadataObject in metadataObjects {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject,
+                  let stringValue = readableObject.stringValue else { return }
+            qrCodes.append(MetadataInfo(corners: readableObject.corners, string: stringValue))
+        }
+        
+        detectionSublayer?.removeFromSuperlayer()
+        let shapeLayer = CAShapeLayer()
+        detectionSublayer = shapeLayer
+        
+        
+        guard let oldCorners = (metadataObjects.first as? AVMetadataMachineReadableCodeObject)?.corners else { return }
+        
+        let rect = translateCornersSpaceIntoViewSpace(corners: oldCorners)
+        let offSet = rect.width/3
+        
+        let bezierPath = UIBezierPath()
+        
+        
+        bezierPath.move(to: CGPoint(x: rect.minX + offSet, y: rect.minY))
+        bezierPath.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        bezierPath.addLine(to: CGPoint(x: rect.minX, y: rect.minY + offSet))
+        
+        bezierPath.move(to: CGPoint(x: rect.maxX - offSet, y: rect.minY))
+        bezierPath.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        bezierPath.addLine(to: CGPoint(x: rect.maxX, y: rect.minY + offSet))
+        
+        bezierPath.move(to: CGPoint(x: rect.minX + offSet, y: rect.maxY))
+        bezierPath.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        bezierPath.addLine(to: CGPoint(x: rect.minX, y: rect.maxY - offSet))
+        
+        bezierPath.move(to: CGPoint(x: rect.maxX - offSet, y: rect.maxY))
+        bezierPath.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        bezierPath.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - offSet))
+        
+        
+        
+        shapeLayer.path = bezierPath.cgPath
+        shapeLayer.strokeColor = UIColor.systemBlue.cgColor
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = 5
+        
+        previewLayer?.addSublayer(shapeLayer)
+        
+    }
+}
+
+extension CodeReaderViewController: AVCapturePhotoCaptureDelegate {
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            print("Error capturing photo: \(error)")
+        } else {
+            guard let data = photo.fileDataRepresentation() else { return }
+            let img = UIImage(data: data)!.cgImage//fixImageOrientation(UIImage(data: data)!).cgImage
+            self.capturedImage = img
+            
+            
+            guard let capturedImage = self.capturedImage,
+                  var finalQRCode = finalQRCode else { return }
+            
+            finalQRCode.corners = finalQRCode.corners.map({ point -> CGPoint in
+                var newPoint = point
+                guard let capturedImage = self.capturedImage else { return .init() }
+                newPoint.x *= CGFloat(capturedImage.width)
+                newPoint.y *= CGFloat(capturedImage.height)
+                return newPoint
+            })
+            
+            finalQRCodeRect = CGRect(x: finalQRCode.corners[0].x,
+                                     y: finalQRCode.corners[0].y,
+                                     width: finalQRCode.corners[2].x - finalQRCode.corners[0].x,
+                                     height: finalQRCode.corners[2].y - finalQRCode.corners[0].y)
+            
+            
+            guard let cgImage = capturedImage.cropping(to: finalQRCodeRect) else { return }
+            finalImage =  UIImage(cgImage: cgImage).rotate(radians: .pi/2)?.cgImage
+            let vc = ARHologramViewController(string: finalQRCode.string, cgImage: finalImage)
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: false)
+            
+        }
+    }
+}
+
+extension UIImage {
+    func rotate(radians: Float) -> UIImage? {
+        var newSize = CGRect(origin: CGPoint.zero, size: self.size).applying(CGAffineTransform(rotationAngle: CGFloat(radians))).size
+        // Trim off the extremely small float value to prevent core graphics from rounding it up
+        newSize.width = floor(newSize.width)
+        newSize.height = floor(newSize.height)
+
+        UIGraphicsBeginImageContextWithOptions(newSize, false, self.scale)
+        let context = UIGraphicsGetCurrentContext()!
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
